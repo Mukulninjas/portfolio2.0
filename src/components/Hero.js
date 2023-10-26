462fd3bb023bde51563f026029e038e1e4f87b00
Mukul Kumar <mukulninjas@gmail.com>
2023-10-26 22:16:56 +0530

    color changes

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -68,10 +68,10 @@ const Hero = () => {
             <nav className="flex justify-between py-5">
                 <div className="text-3xl md:text-5xl font-extrabold text-slate-600 font-serif dark:text-slate-200">NINJAS <b>.</b></div>
                 <ul className="flex space-x-5 text-2xl">
-                    <li className="hidden lg:block font-semibold text-slate-600 font-serif dark:text-slate-200 transition ease-in-out delay-100 hover:-translate-y-1 hover:scale-110 hover:text-purple-600 duration-200"><a href="#home">Home</a></li>
-                    <li className="hidden lg:block font-semibold text-slate-600 font-serif dark:text-slate-200 transition ease-in-out delay-100 hover:-translate-y-1 hover:scale-110 hover:text-purple-600 duration-200"><a href="#bio">Bio</a></li>
-                    <li className="hidden lg:block font-semibold text-slate-600 font-serif dark:text-slate-200 transition ease-in-out delay-100 hover:-translate-y-1 hover:scale-110 hover:text-purple-600 duration-200"><a href="#about">About</a></li>
-                    <li className="hidden lg:block font-semibold text-slate-600 font-serif dark:text-slate-200 transition ease-in-out delay-100 hover:-translate-y-1 hover:scale-110 hover:text-purple-600 duration-200"><a href="#project">Project</a></li>
+                    <li className="hidden lg:block font-semibold text-slate-500 font-serif transition ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-gradient-to-r hover:from-purple-600 hover:to-blue-600 hover:inline-block hover:text-transparent hover:bg-clip-text duration-200"><a href="#home">Home</a></li>
+                    <li className="hidden lg:block font-semibold text-slate-500 font-serif transition ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-gradient-to-r hover:from-purple-600 hover:to-blue-600 hover:inline-block hover:text-transparent hover:bg-clip-text duration-200"><a href="#bio">Bio</a></li>
+                    <li className="hidden lg:block font-semibold text-slate-500 font-serif transition ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-gradient-to-r hover:from-purple-600 hover:to-blue-600 hover:inline-block hover:text-transparent hover:bg-clip-text duration-200"><a href="#about">About</a></li>
+                    <li className="hidden lg:block font-semibold text-slate-500 font-serif transition ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-gradient-to-r hover:from-purple-600 hover:to-blue-600 hover:inline-block hover:text-transparent hover:bg-clip-text duration-200"><a href="#project">Project</a></li>
                     <li>
                         <button
                             onClick={() => handleThemeSwitch()}
@@ -85,29 +85,29 @@ const Hero = () => {
             </nav>
 
             <div className="m-auto">
-                <div className="flex flex-col md:flex-row">
-                    <div className="w-full md:w-1/2 flex justify-center align-middle">
-                        <img src={mukul} />
+                <div className="flex flex-col md:flex-row items-center">
+                    <div className="w-full md:w-1/2 ">
+                        <img className="motion-reduce:animate-bounce" src={mukul} />
                     </div>
                     <div className="w-full md:w-1/2 space-y-3">
                         <span className="text-4xl font-semibold text-slate-600 dark:text-slate-100">Hello<span className="font-extrabold text-purple-600">_</span></span>
-                        <h1 className="text-slate-700 font-extrabold h-32 text-5xl dark:text-slate-100">I'm <span className="text-purple-600" id="typewriter">Software Engineer</span></h1>
+                        <h1 className="text-slate-700 font-extrabold h-32 text-5xl dark:text-slate-100">I'm <span className="bg-gradient-to-r from-purple-600 to-blue-600 inline-block text-transparent bg-clip-text" id="typewriter">Software Engineer</span></h1>
                         <p className="text-2xl text-slate-600 dark:text-slate-100">Software Engineering graduate and a Coding Enthusiast with strong technical skills anchored
                             on
                             extensive engineering coursework and a special focus on Full Stack Web Development. Let's
                             create something
                             extraordinary together!</p>
                         <div className="">
-                            <button className="w-52 h-16 bg-slate-600 shadow-xl shadow-slate-600 dark:shadow-slate-950 text-slate-50 text-2xl font-semibold font-serif transition ease-in-out delay-100 hover:-translate-y-1 hover:scale-110 hover:bg-purple-600 duration-200"><a
+                            <button className="w-52 h-16 bg-slate-600 shadow-xl shadow-slate-600 dark:shadow-slate-950 text-slate-50 text-2xl font-semibold font-serif transition ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-gradient-to-r hover:from-purple-600 hover:to-blue-600 duration-200"><a
                                 href="https://drive.google.com/file/d/19uWjImRV4P4tfPLPZxIMVeG5WQUDpE19/view?usp=drivesdk"
                                 target="_blank"
                                 download>Download CV</a></button>
                         </div>
-                        <section className="space-x-5 text-5xl text-purple-600 py-10">
-                            <a href="https://www.linkedin.com/in/mukul-kumar-31a2a71b0/"><i className="fab fa-linkedin transition ease-in-out delay-100 hover:-translate-y-1 hover:scale-110 hover:text-purple-900 duration-200"></i></a>
-                            <a href="https://github.com/Mukulninjas"><i className="fab fa-github-square transition ease-in-out delay-100 hover:-translate-y-1 hover:scale-110 hover:text-purple-900 duration-200"></i></a>
-                            <a href="mailto:mukulninjas@gmail.com"><i className="fa fa-envelope transition ease-in-out delay-100 hover:-translate-y-1 hover:scale-110 hover:text-purple-900 duration-200"></i></a>
-                            <a href="tel:9582688985"><i className="fa fa-phone transition ease-in-out delay-100 hover:-translate-y-1 hover:scale-110 hover:text-purple-900 duration-200"></i></a>
+                        <section className="space-x-5 text-5xl bg-gradient-to-r from-purple-600 to-blue-600 inline-block text-transparent bg-clip-text py-10">
+                            <a href="https://www.linkedin.com/in/mukul-kumar-31a2a71b0/"><i className="fab fa-linkedin transition ease-in-out delay-100 hover:-translate-y-1 hover:scale-110 hover:bg-gradient-to-r hover:from-purple-600 hover:to-blue-600 hover:inline-block hover:text-transparent hover:bg-clip-text duration-200"></i></a>
+                            <a href="https://github.com/Mukulninjas"><i className="fab fa-github-square transition ease-in-out delay-100 hover:-translate-y-1 hover:scale-110 hover:bg-gradient-to-r hover:from-purple-600 hover:to-blue-600 hover:inline-block hover:text-transparent hover:bg-clip-text duration-200"></i></a>
+                            <a href="mailto:mukulninjas@gmail.com"><i className="fa fa-envelope transition ease-in-out delay-100 hover:-translate-y-1 hover:scale-110 hover:bg-gradient-to-r hover:from-purple-600 hover:to-blue-600 hover:inline-block hover:text-transparent hover:bg-clip-text duration-200"></i></a>
+                            <a href="tel:9582688985"><i className="fa fa-phone transition ease-in-out delay-100 hover:-translate-y-1 hover:scale-110 hover:bg-gradient-to-r hover:from-purple-600 hover:to-blue-600 hover:inline-block hover:text-transparent hover:bg-clip-text duration-200"></i></a>
                         </section>
                     </div>
                 </div>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -43,8 +43,8 @@ const Projects = () => {
                   <div className="text-xl">{index.subtitle}</div>
                   </div>
                 <div className="flex justify-between">
-                  <div className="w-32 text-center rounded-lg bg-slate-800 p-2 text-lg text-slate-200 transition ease-in-out delay-100 hover:-translate-y-1 hover:scale-110 hover:bg-purple-600 duration-200"><a href={index.source_code} target="_blank">Source Code</a></div>
-                  <div className="w-32 text-center rounded-lg bg-slate-800 p-2 text-lg text-slate-200 transition ease-in-out delay-100 hover:-translate-y-1 hover:scale-110 hover:bg-purple-600 duration-200"><a href={index.live} target="_blank">Live</a></div>
+                  <div className="w-32 text-center rounded-lg bg-slate-800 p-2 text-lg text-slate-200 transition ease-in-out delay-100 hover:-translate-y-1 hover:scale-110 hover:bg-gradient-to-r hover:from-purple-600 hover:to-blue-600 duration-200"><a href={index.source_code} target="_blank">Source Code</a></div>
+                  <div className="w-32 text-center rounded-lg bg-slate-800 p-2 text-lg text-slate-200 transition ease-in-out delay-100 hover:-translate-y-1 hover:scale-110 hover:bg-gradient-to-r hover:from-purple-600 hover:to-blue-600 duration-200"><a href={index.live} target="_blank">Live</a></div>
                 </div>
               </div>)
           })}
